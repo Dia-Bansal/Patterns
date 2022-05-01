98749bc1014da38b44ab06d2ace1cf67fff8c139
Dia-Bansal <78194499+Dia-Bansal@users.noreply.github.com>
2022-05-01 18:44:14 +0530

    Print Square Pattern counting in reverse order

---
+++ SquarePattern4.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int count=n*n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<count<<" ";
+            count--;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
